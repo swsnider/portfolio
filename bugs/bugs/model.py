6c89e8acd163b4051a53ea64434e4556d0d97a83
Silas Snider <swsnider@gmail.com>
2008-03-12 01:59:48 -0700

    added grid css

--- bugs/bugs/model.py
+++ bugs/bugs/model.py
@@ -2,10 +2,10 @@ from datetime import datetime
 from turbogears.database import PackageHub
 # import some basic SQLObject classes for declaring the data model
 # (see http://www.sqlobject.org/SQLObject.html#declaring-the-class)
-from sqlobject import SQLObject, SQLObjectNotFound, RelatedJoin
+from sqlobject import SQLObject, SQLObjectNotFound, RelatedJoin, MultipleJoin, ForeignKey
 # import some datatypes for table columns from SQLObject
 # (see http://www.sqlobject.org/SQLObject.html#column-types for more)
-from sqlobject import StringCol, UnicodeCol, IntCol, DateTimeCol
+from sqlobject import StringCol, UnicodeCol, IntCol, DateTimeCol, BoolCol, EnumCol
 from turbogears import identity
 
 __connection__ = hub = PackageHub('bugs')
